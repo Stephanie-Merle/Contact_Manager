23185c36cd384fae2998dffc7552632cbc75ef04
Stephanie Merle <merle.stephany@gmail.com>
2019-08-17 18:38:21 +0200

    english translation and press enter event

--- js/contacts.js
+++ js/contacts.js
@@ -1,20 +1,20 @@
 /* 
-Activité : gestion des contacts
-Auteur: Stéphanie Merle
+Contact management
+Author: Stéphanie Merle
 */
 
-// Create class
+// Create a class named Contacts
 class Contacts{
-  constructor(prenom, nom){
-    this.prenom = prenom;
-    this.nom = nom;
+  constructor(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
   print(){
-    console.log(`Nom: ${this.nom}, prénom: ${this.prenom}`);
+    console.log(`Last Name: ${this.lastName}, First Name: ${this.firstName}`);
   }
 };
 
-// Create and push contacts in Array
+// Create and push the 2 initial contacts in an array named contactList
 const contact1 = new Contacts("Carole","Lévisse");
 const contact2 = new Contacts("Mélodie","Nelsonne");
 let contactList = [];
@@ -23,16 +23,30 @@ contactList.push(contact1, contact2);
 let theName ="";
 let theSurname ="";
 
-const menuList = "\n1: Lister les contacts \n2: Ajouter un contact \n0: Quitter";
-console.log(`Bienvenue dans le gestionnaire de contact ${menuList}`);
+const menuList = "\n1: Your contact list \n2: Add a new contact \n0: Quit";
+console.log(`Welcome to contact management ${menuList}`);
+const choice = 'Choose an option:';
 
+// event listener on click OK
 document.getElementById("validation").onclick = menu;
+
+// event listenenr on press ENTER simulate click OK event
+document.getElementById("inputValue").addEventListener("keyup", function(event) {
+  // Enter key === 13
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the click button
+    document.getElementById("validation").click();
+  }
+});
+
 // Choice 1 || 2 || 0
 function menu(){
   let inputValue = document.getElementById("inputValue").value;
   document.getElementById("inputValue").value = "";
   if(inputValue === "1"){
-    console.log("Voici la liste de tous vos contacts:");
+    console.log("Your contact list:");
     for(contact of contactList){
       contact.print();
     } 
@@ -40,44 +54,47 @@ function menu(){
   }else if(inputValue === "2"){
     creationContact();
   }else if(inputValue === "0"){
-    console.log("Aurevoir");
-    // fin programme
+    console.log("Goodbye");
+    // break
     document.write('');
   }else{
-    console.log("Valeur non acceptee");
+    console.log("Value not accepted");
     document.getElementById("inputValue").value = "";
     console.log(menuList);
   }
 };
 
-// Input "2" create a new contact
+// Input "2" waiting for new contact first name
 function creationContact(){
-  document.getElementById("question").innerHTML = "Le Prenom de votre nouveau contact:" ;
+  document.getElementById("question").innerHTML = "New contact first name:" ;
   document.getElementById("inputValue").value = "";
-  document.getElementById("validation").onclick = pushPrenom;
+  document.getElementById("validation").onclick = pushFirstName;
 };
 
-function pushPrenom(){
+// Get first name, waiting for last name
+function pushFirstName(){
   theName = document.getElementById("inputValue").value;
   document.getElementById("inputValue").value = "";
-  document.getElementById("question").innerHTML = "Le Nom de votre nouveau contact:" ;
-  document.getElementById("validation").onclick = pushNom;
+  document.getElementById("question").innerHTML = "New contact last name:" ;
+  document.getElementById("validation").onclick = pushLastName;
 };
 
-function pushNom(){
+// Get last name, confirm the new contact creation and reload menu choice
+function pushLastName(){
   theSurname = document.getElementById("inputValue").value;
   document.getElementById("inputValue").value = "";
-  console.log(`Vous avez un nouveau contact:  ${theName} ${theSurname}`);
+  console.log(`You added a new contact:  ${theName} ${theSurname}`);
   let newContact = new Contacts(theName, theSurname);
   contactList.push(newContact);
   console.log(menuList);
-  document.getElementById("question").innerHTML ="Choisissez une option:";
+  document.getElementById("question").innerHTML = choice;
   document.getElementById("validation").onclick = menu;
 };
 
+// If click on CANCEL
 function reset(){
   document.getElementById("inputValue").value = "";
-  document.getElementById("question").innerHTML ="Choisissez une option:";
+  document.getElementById("question").innerHTML = choice;
   console.log(menuList);
   document.getElementById("validation").onclick = menu;
 }
